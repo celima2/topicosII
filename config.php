320595d3b18807f12969a50555d632007b510aa8
celima2 <anaceciliaup@gmail.com>
2022-10-26 17:44:29 -0300

    login do banco no heroku

--- config.php
+++ config.php
@@ -3,13 +3,22 @@
 // Sample file: Never send your credentials to git
 
 // host
-$host = 'localhost';
+/*$host = 'localhost';
 
 // db
 $db_name = 'topicosii';
 $db_host = 'localhost';
 $db_user = 'root';
-$db_pass = '';
+$db_pass = '';*/
+
+// host
+$host = 'us-cdbr-east-06.cleardb.net';
+
+// db
+$db_name = 'topicosii';
+$db_host = 'us-cdbr-east-06.cleardb.net';
+$db_user = 'bb3c13a576048d';
+$db_pass = '168d1fab';
 
 try {
   mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
